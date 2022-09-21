441d9d79f0d6b32b69dd46794f9d839b4100d384
yltan <yaolongt@gmail.com>
2022-09-21 15:03:13 +0800

    added PlannedRoutes model and repo

---
+++ src/main/java/com/bussin/SpringBack/repositories/PlannedRoutesRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bussin.SpringBack.models.PlannedRoutes;
+
+public interface PlannedRoutesRepository extends JpaRepository<PlannedRoutes, String> {
+
+}
