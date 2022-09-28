4a504056077bce8576e173c3f198d0a64ba6ea5a
Yao Long <102669835+yaolongt@users.noreply.github.com>
2022-09-28 11:04:15 +0800

    feature/Ride unit tests (#18)
    * Ride Tests got problem
    
    * Fixed Tests
    
    * basic ride tests
    
    Co-authored-by: Matthew Ho <18333157+mehtho@users.noreply.github.com>
    Co-authored-by: Matt <matthewho.2021@scis.smu.edu.sg>

--- src/main/java/com/bussin/SpringBack/models/Ride.java
+++ src/main/java/com/bussin/SpringBack/models/Ride.java
@@ -1,17 +1,29 @@
 package com.bussin.SpringBack.models;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
-
-import java.io.*;
-import java.math.*;
-import java.sql.*;
-import java.util.*;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Objects;
+import java.util.UUID;
 
 @Entity
 @Builder
@@ -51,5 +63,19 @@ public class Ride implements Serializable {
         this.id = rideDTO.getId();
         this.timestamp = rideDTO.getTimestamp();
         this.passengers = rideDTO.getPassengers();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        Ride ride = (Ride) o;
+        return id != null && Objects.equals(id, ride.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
